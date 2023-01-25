958c91154e516bc89bd46dfb58e1faca7a5eccc1
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-01-25 23:14:43 +0530

    [local] Setup logger

--- local/src/extension.ts
+++ local/src/extension.ts
@@ -1,7 +1,11 @@
 import * as vscode from 'vscode';
+import { setLogger as setRemoteAdbLogger } from 'remote-adb';
 import { RemoteAndroidTreeDataProvider } from './remoteAndroidDevices';
+import { logger } from './logger';
 
 export function activate(context: vscode.ExtensionContext) {
+	setRemoteAdbLogger(logger);
+
 	let treeView = vscode.window.createTreeView("remote-android", {
 		"treeDataProvider": new RemoteAndroidTreeDataProvider(),
 	});
