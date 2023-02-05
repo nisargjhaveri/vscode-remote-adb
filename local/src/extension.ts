009d4296c10c02533eeff2eb350f584af937839d
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-02-05 12:40:39 +0530

    [local] Allow adding arbitary TCP devices

--- local/src/extension.ts
+++ local/src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { setLogger as setRemoteAdbLogger } from 'remote-adb';
+import { setLogger as setRemoteAdbLogger, TcpDeviceManager } from 'remote-adb';
 import { RemoteAndroidTreeDataProvider, RemoteAndroidTreeItem } from './remoteAndroidDevices';
 import { logger } from './logger';
 import { getServerConnection } from './serverConnection';
@@ -33,6 +33,22 @@ export function activate(context: vscode.ExtensionContext) {
 
 		if (device.connected) {
 			await device.disconnect();
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remote-android.addTcpDevice', async () => {
+		let serial = await vscode.window.showInputBox({
+			"title": "Add a device via TCP/IP",
+			"placeHolder": "e.g. 192.168.0.2:5555",
+			"prompt": "HOST:PORT for the device to connect",
+		});
+
+		if (!serial) { return; }
+
+		let device = await TcpDeviceManager.createDevice(serial);
+
+		if (!device) {
+			throw new Error(`Cannot connect to '${serial}'. Please enter a valid hostname and port to connect`);
 		}
 	}));
 }
